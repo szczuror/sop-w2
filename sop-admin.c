657a090807030d25fff8ba8f643c5f45888090ef
Franciszek Jełowicki <franciszek.jelowicki@pw.edu.pl>
2025-11-17 20:36:59 +0100

    w2pl init

---
+++ sop-admin.c
@@ -0,0 +1,50 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ERR(source) \
+    (kill(0, SIGKILL), perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+#define FILE_MAX_SIZE 512
+
+ssize_t bulk_read(int fd, char* buf, size_t count)
+{
+    ssize_t c;
+    ssize_t len = 0;
+    do
+    {
+        c = TEMP_FAILURE_RETRY(read(fd, buf, count));
+        if (c < 0)
+            return c;
+        if (c == 0)
+            return len;  // EOF
+        buf += c;
+        len += c;
+        count -= c;
+    } while (count > 0);
+    return len;
+}
+
+void usage(int argc, char* argv[])
+{
+    printf("%s p h\n", argv[0]);
+    printf("\tp - path to directory describing the structure of the Austro-Hungarian office in Prague.\n");
+    printf("\th - Name of the highest administrator.\n");
+    exit(EXIT_FAILURE);
+}
+
+void sethandler(void (*f)(int), int sigNo)
+{
+    struct sigaction act;
+    memset(&act, 0, sizeof(struct sigaction));
+    act.sa_handler = f;
+    if (-1 == sigaction(sigNo, &act, NULL))
+        ERR("sigaction");
+}
+
+int main(int argc, char* argv[]) {}

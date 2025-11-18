74fbca710c68db21e36007bc0c2db8c08803d815
Igor Kowalik <01199301@pw.edu.pl>
2025-11-18 16:33:50 +0100

    s

--- sop-admin.c
+++ sop-admin.c
@@ -1,9 +1,14 @@
 #define _GNU_SOURCE
+
+#include <bits/types/sigset_t.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -38,6 +43,9 @@ void usage(int argc, char* argv[])
     exit(EXIT_FAILURE);
 }
 
+volatile sig_atomic_t sig_usr1 = 0;
+void sigusr1_handler(int sig) { sig_usr1 = sig; }
+
 void sethandler(void (*f)(int), int sigNo)
 {
     struct sigaction act;
@@ -47,4 +55,55 @@ void sethandler(void (*f)(int), int sigNo)
         ERR("sigaction");
 }
 
-int main(int argc, char* argv[]) {}
+void e2(char* path, char* name) {
+    printf("My name is %s and my PID is %d\n", name, getpid());
+    char cwd[FILE_MAX_SIZE];
+    getcwd(cwd, FILE_MAX_SIZE);
+    chdir(path);
+    // sprintf(path + strlen(path), "%s", name);
+    // printf("%s\n", path);
+    int fd = open(name, O_RDONLY);
+    if (fd == -1)
+        ERR("open");
+    char file_buf[FILE_MAX_SIZE];
+    bulk_read(fd, file_buf, FILE_MAX_SIZE);
+    char* linia = strtok(file_buf, "\n");
+    while(linia != NULL){
+        printf("%s inspecting %s\n", name, linia);
+        linia = strtok(NULL, "\n");
+        if(strcmp(linia, "-")){
+        if(fork()){
+            e2(".", linia);
+        }
+        break;
+        }
+    }
+    printf("%s has inspected all subordinates\n", name);
+    chdir(cwd);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+        usage(argc, argv);
+    
+    char *path = argv[1];
+    char *name = argv[2];
+
+    sigset_t mask, oldmask;
+    sigemptyset(&mask);
+
+    sigaddset(&mask, SIGUSR1);
+    sigaddset(&mask, SIGUSR2);
+    sigaddset(&mask, SIGINT);
+
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+    sethandler(sigusr1_handler, SIGUSR1);
+
+    printf("Waiting for SIGUSR1\n");
+    // sigsuspend(&oldmask);
+    printf("Got SIGUSR1\n");
+
+    e2(path, name);
+}

a02bdf6299412d4abdf48cae0ccc3a7cd441509b
Igor Kowalik <igik451@gmail.com>
2025-11-23 17:53:10 +0100

    all done

--- sop-admin.c
+++ sop-admin.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <sys/select.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #define ERR(source) \
@@ -43,8 +44,14 @@ void usage(int argc, char* argv[])
     exit(EXIT_FAILURE);
 }
 
-volatile sig_atomic_t sig_usr1 = 0;
-void sigusr1_handler(int sig) { sig_usr1 = sig; }
+volatile sig_atomic_t last_sig = 0;
+volatile sig_atomic_t sflag = 0;
+void sigusr1_handler(int sig)
+{
+    last_sig = sig;
+    if (sig == SIGINT)
+        sflag = 1;
+}
 
 void sethandler(void (*f)(int), int sigNo)
 {
@@ -55,8 +62,11 @@ void sethandler(void (*f)(int), int sigNo)
         ERR("sigaction");
 }
 
-void process(char* path, char* name) {
+void process(char* path, char* name, int isRoot)
+{
     printf("My name is %s and my PID is %d\n", name, getpid());
+
+    srand(time(NULL) * getpid());
 
     char fullPath[PATH_MAX];
     snprintf(fullPath, PATH_MAX, "%s/%s", path, name);
@@ -67,41 +77,94 @@ void process(char* path, char* name) {
 
     char file_buf[FILE_MAX_SIZE + 1];
     ssize_t bytes_read = bulk_read(fd, file_buf, FILE_MAX_SIZE);
-    if(bytes_read < 0) ERR("bulk_read");
+    if (bytes_read < 0)
+        ERR("bulk_read");
     file_buf[bytes_read] = '\0';
-    if(close(fd) == -1) ERR("close");
+    if (close(fd) == -1)
+        ERR("close");
 
     char* linia = strtok(file_buf, "\n");
-    while(linia != NULL){
-        if(strcmp(linia, "-") == 0) break;
-        
-        printf("%s inspecting %s\n", name, linia);
+    while (linia != NULL)
+    {
+        if (strcmp(linia, "-") != 0)
+        {
+            printf("%s inspecting %s\n", name, linia);
 
-        pid_t pid = fork();
+            pid_t pid = fork();
 
-        if(pid == -1) ERR("fork");
+            if (pid == -1)
+                ERR("fork");
 
-        if(pid == 0) {
-            process(path, linia);
-            exit(EXIT_SUCCESS);
+            if (pid == 0)
+            {
+                process(path, linia, 0);  // dziecko nie jest rootem, wiec 0
+                exit(EXIT_SUCCESS);
+            }
         }
 
         linia = strtok(NULL, "\n");
-        }
-        
-        printf("%s has inspected all subordinates\n", name);
-        while(wait(NULL) > 0);
-        printf("%s is leaving the office\n", name);
+    }
 
+    printf("%s has inspected all subordinates\n", name);
+
+    // oczekiwanie na SIGUSR2
+    sigset_t suspendMask;
+    sigemptyset(&suspendMask);
+
+    while (1)
+    {
+        if (sflag == 1)
+            break;
+        sigsuspend(&suspendMask);
+        if (sflag == 1)
+            break;
+        if (last_sig == SIGUSR2)
+        {
+            last_sig = 0;
+
+            int action = rand() % 3;
+
+            if (action == 0)
+            {
+                printf("%s received a document. Sending to the archive\n", name);
+            }
+            else
+            {
+                if (isRoot == 1)
+                {
+                    printf("%s received a document. Ignoring\n", name);
+                }
+                else
+                {
+                    printf("%s received a document. Passing on to the superintendent\n", name);
+                    if (kill(getppid(), SIGUSR2) == -1)
+                        ERR("kill");
+                }
+            }
+        }
+    }
+
+    printf("%s ending the day.\n", name);
+
+    sethandler(SIG_IGN, SIGINT);  // sigint zabije dzieci a nie sam proces ktory dostaje sygnal
+
+    kill(0, SIGINT);
+
+    while (wait(NULL) > 0)
+        ;
+
+    printf("%s is leaving the office\n", name);
+
+    exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char* argv[])
 {
     if (argc != 3)
         usage(argc, argv);
-    
-    char *path = argv[1];
-    char *name = argv[2];
+
+    char* path = argv[1];
+    char* name = argv[2];
 
     sigset_t mask, oldmask;
     sigemptyset(&mask);
@@ -113,12 +176,15 @@ int main(int argc, char* argv[])
     sigprocmask(SIG_BLOCK, &mask, &oldmask);
 
     sethandler(sigusr1_handler, SIGUSR1);
+    sethandler(sigusr1_handler, SIGUSR2);
+    sethandler(sigusr1_handler, SIGINT);
 
-    // printf("Waiting for SIGUSR1\n");
-    // sigsuspend(&oldmask);
-    // printf("Got SIGUSR1\n");
-    // if(sig_usr1 == SIGUSR1){
-        process(path, name);
-    // }
-
+    printf("Waiting for SIGUSR1\n");
+    sigsuspend(&oldmask);
+    printf("Got SIGUSR1\n");
+    if (last_sig == SIGUSR1)
+    {
+        last_sig = 0;
+        process(path, name, 1);
+    }
 }
